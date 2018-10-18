48b5f54d19f2f5d0c1155c8f3ad134e1d74bfd18
Roy Bao <roy@roybao.com>
2018-10-17 17:44:48 -0700

    Chore: revert Struct.merge

--- base/fc_support/lib/fc_support/struct.ex
+++ base/fc_support/lib/fc_support/struct.ex
@@ -1,27 +1,17 @@
 defmodule FCSupport.Struct do
   @doc """
-  Merge the `src` struct or map into the `dest` struct. Keys that are not part of `dest`
+  Merge the `src` struct into the `dest` struct. Keys that are not part of `dest`
   will be safely ignored.
-
-  If the given `src` is a map with string keys, it will be treated as if the keys
-  are given as atom. It will do the conversion safely only convert if the `dest`
-  has a matching atom key.
-
   This function is different than `struct/2` as that function will raise if the
   second argument is a struct.
   """
-  @spec merge(struct, struct | map, keyword) :: struct
   def merge(dest, src, opts \\ []) do
     keys = opts[:only] || Map.keys(dest) -- [:__struct__]
-    keys = Enum.map(keys, &Atom.to_string/1)
-
     excepts = opts[:except] || []
-    excepts = Enum.map(excepts, &Atom.to_string/1)
-
     keys = keys -- excepts
-    safe_src = Map.take(stringify_keys(src), keys)
 
-    struct(dest, atomize_keys(safe_src))
+    filtered_src = Map.take(src, keys)
+    struct(dest, filtered_src)
   end
 
   @doc """
@@ -30,20 +20,6 @@ defmodule FCSupport.Struct do
   """
   def merge_to(src, dest, opts \\ []) do
     merge(dest, src, opts)
-  end
-
-  def stringify_keys(%{__struct__: _} = s) do
-    stringify_keys(Map.from_struct(s))
-  end
-
-  def stringify_keys(m) when is_map(m) do
-    Enum.reduce(m, %{}, fn({k, v}, acc) ->
-      if is_atom(k) do
-        Map.put(acc, Atom.to_string(k), v)
-      else
-        Map.put(acc, k, v)
-      end
-    end)
   end
 
   def atomize_keys(m, permitted \\ nil) do
