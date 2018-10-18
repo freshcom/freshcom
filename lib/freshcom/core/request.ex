10a644159a376f3dce88a8c1c80f23b3b1dde52c
Roy Bao <roy@roybao.com>
2018-10-17 17:45:16 -0700

    Chore: rename :requester_id to just :id on Request struct

--- lib/freshcom/core/request.ex
+++ lib/freshcom/core/request.ex
@@ -2,7 +2,7 @@ defmodule Freshcom.Request do
   use TypedStruct
 
   typedstruct do
-    field :requester, map(), default: %{requester_id: nil, account_id: nil}
+    field :requester, map(), default: %{id: nil, account_id: nil}
     field :fields, map(), default: %{}
     field :identifiers, map(), default: %{}
     field :locale, String.t()
