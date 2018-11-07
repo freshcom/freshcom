e1b61e92af955c51fe825e8273441ab67817a309
Roy Bao <roy@roybao.com>
2018-11-07 12:17:32 -0800

    Fix: RefreshToken.prefix_id should work even if account is given as nil

--- lib/freshcom/projections/refresh_token.ex
+++ lib/freshcom/projections/refresh_token.ex
@@ -1,6 +1,9 @@
 defmodule Freshcom.RefreshToken do
   use Freshcom.Projection
 
+  alias Freshcom.Repo
+  alias Freshcom.Account
+
   schema "refresh_tokens" do
     field :account_id, UUID
     field :user_id, UUID
@@ -10,11 +13,14 @@ defmodule Freshcom.RefreshToken do
   end
 
   def prefixed_id(nil, _), do: nil
+  def prefixed_id(rt, nil), do: prefixed_id(rt, Repo.get!(Account, rt.account_id))
   def prefixed_id(%{id: id, user_id: nil}, %{mode: mode}), do: "prt-#{mode}-#{id}"
   def prefixed_id(%{id: id}, %{mode: mode}), do: "urt-#{mode}-#{id}"
 
   def put_prefixed_id(nil, _), do: nil
   def put_prefixed_id(rt, acct), do: %{rt | prefixed_id: prefixed_id(rt, acct)}
+
+  def unprefix_id(nil), do: nil
 
   def unprefix_id(id) do
     id
