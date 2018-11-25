ce2cbb7824098a868f311dc92b2e4d8add48e5ae
Roy Bao <roy@roybao.com>
2018-11-25 00:31:18 -0800

    Fix(fc_identity): nil username should pass uniqueness test

--- services/fc_identity/lib/fc_identity/command_validator.ex
+++ services/fc_identity/lib/fc_identity/command_validator.ex
@@ -1,6 +1,8 @@
 defmodule FCIdentity.CommandValidator do
   alias FCIdentity.UsernameStore
 
+  def unique_username(nil, _), do: :ok
+
   def unique_username(username, cmd) do
     unless UsernameStore.exist?(username, Map.get(cmd, :account_id)) do
       :ok
