6c186e079539b080b203597ebef39107264459a0
Roy Bao <roy@roybao.com>
2018-12-12 11:30:54 -0800

    Feature(fc_base): add client identification middleware

---
+++ base/fc_base/lib/fc_base/middlewares/client_identification.ex
@@ -0,0 +1,37 @@
+defmodule FCBase.ClientIdentification do
+  @behaviour Commanded.Middleware
+
+  alias Commanded.Middleware.Pipeline
+  alias FCStateStorage.GlobalStore.AppStore
+
+  def before_dispatch(%Pipeline{} = pipeline) do
+    %{pipeline | command: identify(pipeline.command)}
+  end
+
+  def identify(%{client_id: nil} = cmd) do
+    %{cmd | client_type: nil}
+  end
+
+  def identify(%{client_id: client_id, account_id: account_id} = cmd) do
+    client = AppStore.get(client_id)
+
+    cond do
+      is_nil(client) ->
+        %{cmd | client_type: nil}
+
+      client.type == "system" ->
+        %{cmd | client_type: client.type}
+
+      client.type == "standard" && client.account_id == account_id ->
+        %{cmd | client_type: client.type}
+
+      true ->
+        %{cmd | client_type: nil}
+    end
+  end
+
+  def identify(cmd), do: cmd
+
+  def after_dispatch(pipeline), do: pipeline
+  def after_failure(pipeline), do: pipeline
+end
