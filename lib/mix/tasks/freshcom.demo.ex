874b1f2b41252e6985c634a7ec9fd825b59e3d81
Roy Bao <roy@roybao.com>
2018-12-12 16:16:35 -0800

    Feature(mix): add a system application for freshcom.demo mix task

--- lib/mix/tasks/freshcom.demo.ex
+++ lib/mix/tasks/freshcom.demo.ex
@@ -8,15 +8,26 @@ defmodule Mix.Tasks.Freshcom.Demo do
     Application.ensure_all_started(:freshcom)
 
     req = %Request{
-        fields: %{
-          "name" => "Demo User",
-          "username" => "test@example.com",
-          "email" => "test@example.com",
-          "password" => "test1234",
-          "is_term_accepted" => true
-        }
-      }
+      fields: %{
+        "name" => "Demo User",
+        "username" => "test@example.com",
+        "email" => "test@example.com",
+        "password" => "test1234",
+        "is_term_accepted" => true
+      },
+      _role_: "system"
+    }
 
     {:ok, _} = Identity.register_user(req)
+
+    req = %Request{
+      fields: %{
+        "type" => "system",
+        "name" => "Freshcom Dashboard"
+      },
+      _role_: "system"
+    }
+
+    {:ok, _} = Identity.add_app(req)
   end
 end
