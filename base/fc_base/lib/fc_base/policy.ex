3b8d0df08b21123c628933fef0a744fa4cded7da
Roy Bao <roy@roybao.com>
2019-01-14 17:46:47 -0800

    Chore(fc_base): remove unused function

--- base/fc_base/lib/fc_base/policy.ex
+++ base/fc_base/lib/fc_base/policy.ex
@@ -1,23 +1,6 @@
 defmodule FCBase.Policy do
   use OK.Pipe
 
-  def only(cmd, :system_app),
-    do: only(cmd, &(&1.client_type == "system"))
-
-  def only(cmd, :standard_user),
-    do: only(cmd, &(&1.requester_type == "standard"))
-
-  def only(cmd, :owner_role),
-    do: only(cmd, &(&1.requester_role == "owner"))
-
-  def only(cmd, condition) do
-    if condition.(cmd) do
-      {:ok, cmd}
-    else
-      {:error, :access_denied}
-    end
-  end
-
   def default(cmd, state, roles) do
     cmd
     |> authorize_by_account(state.account_id)
