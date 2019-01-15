4075357abcd3f9f1f337798c1eacd40d75348c11
Roy Bao <roy@roybao.com>
2019-01-15 15:53:39 -0800

    Fix(fc_base): manager should be part of customer management roles

--- base/fc_base/lib/fc_base.ex
+++ base/fc_base/lib/fc_base.ex
@@ -5,7 +5,7 @@ defmodule FCBase do
 
       @admin_roles ["owner", "administrator"]
       @dev_roles ["owner", "administrator", "developer"]
-      @customer_management_roles ["owner", "administrator", "support_specialist"]
+      @customer_management_roles ["owner", "administrator", "manager", "support_specialist"]
 
       def authorize(%{requester_role: "sysdev"} = cmd, _), do: {:ok, cmd}
       def authorize(%{requester_role: "system"} = cmd, _), do: {:ok, cmd}
