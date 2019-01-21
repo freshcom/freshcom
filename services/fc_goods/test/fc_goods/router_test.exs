4fd655fda9c0d6ea9f89d7bd117568a4231fae01
Roy Bao <roy@roybao.com>
2019-01-20 23:53:05 -0800

    Chore(fc_goods): use assert for clearer test

--- services/fc_goods/test/fc_goods/router_test.exs
+++ services/fc_goods/test/fc_goods/router_test.exs
@@ -25,12 +25,12 @@ defmodule FCGoods.RouterTest do
     end
 
     test "given invalid command" do
-      {:error, {:validation_failed, errors}} = Router.dispatch(%AddStockable{})
+      assert {:error, {:validation_failed, errors}} = Router.dispatch(%AddStockable{})
       assert length(errors) > 0
     end
 
     test "given valid command with unauthorized role", %{cmd: cmd} do
-      {:error, :access_denied} = Router.dispatch(cmd)
+      assert {:error, :access_denied} = Router.dispatch(cmd)
     end
 
     test "given valid command with authorized role", %{cmd: cmd} do
@@ -39,16 +39,15 @@ defmodule FCGoods.RouterTest do
       client_id = app_id("standard", account_id)
 
       cmd = %{cmd | client_id: client_id, account_id: account_id, requester_id: requester_id}
-      :ok = Router.dispatch(cmd)
 
+      assert :ok = Router.dispatch(cmd)
       assert_receive_event(StockableAdded, fn(event) ->
         assert event.name == cmd.name
       end)
     end
 
     test "given valid command with system role", %{cmd: cmd} do
-      :ok = Router.dispatch(%{cmd | requester_role: "system"})
-
+      assert :ok = Router.dispatch(%{cmd | requester_role: "system"})
       assert_receive_event(StockableAdded, fn(event) ->
         assert event.name == cmd.name
       end)
