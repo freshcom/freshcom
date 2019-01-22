fa1e076293e3669359915bc028bcdd4c3bebe34a
Roy Bao <roy@roybao.com>
2019-01-22 13:59:24 -0800

    Refactor(fc_goods): improve test

--- services/fc_goods/test/fc_goods/router_test.exs
+++ services/fc_goods/test/fc_goods/router_test.exs
@@ -61,6 +61,7 @@ defmodule FCGoods.RouterTest do
   describe "dispatch UpdateStockable" do
     setup do
       cmd = %UpdateStockable{
+        stockable_id: uuid4(),
         name: Faker.Commerce.product_name()
       }
 
@@ -73,42 +74,40 @@ defmodule FCGoods.RouterTest do
     end
 
     test "given non existing stockable id", %{cmd: cmd} do
-      assert {:error, {:not_found, :stockable}} = Router.dispatch(%{cmd | stockable_id: uuid4()})
+      assert {:error, {:not_found, :stockable}} = Router.dispatch(cmd)
     end
 
     test "given valid command with unauthorized role", %{cmd: cmd} do
-      stockable_id = uuid4()
       to_streams("stockable", [
         %StockableAdded{
           client_id: uuid4(),
           account_id: uuid4(),
           requester_id: uuid4(),
-          stockable_id: stockable_id,
+          stockable_id: cmd.stockable_id,
           name: Faker.Commerce.product_name(),
           unit_of_measure: "EA"
         }
       ])
 
-      assert {:error, :access_denied} = Router.dispatch(%{cmd | stockable_id: stockable_id})
+      assert {:error, :access_denied} = Router.dispatch(cmd)
     end
 
     test "given valid command with authorized role", %{cmd: cmd} do
       account_id = uuid4()
       client_id = app_id("standard", account_id)
       requester_id = user_id(account_id, "goods_specialist")
-      stockable_id = uuid4()
       to_streams("stockable", [
         %StockableAdded{
           client_id: client_id,
           account_id: account_id,
           requester_id: requester_id,
-          stockable_id: stockable_id,
+          stockable_id: cmd.stockable_id,
           name: Faker.Commerce.product_name(),
           unit_of_measure: "EA"
         }
       ])
 
-      cmd = %{cmd | client_id: client_id, account_id: account_id, requester_id: requester_id, stockable_id: stockable_id}
+      cmd = %{cmd | client_id: client_id, account_id: account_id, requester_id: requester_id}
 
       assert :ok = Router.dispatch(cmd)
       assert_receive_event(StockableUpdated, fn(event) ->
@@ -117,16 +116,15 @@ defmodule FCGoods.RouterTest do
     end
 
     test "given valid command with system role", %{cmd: cmd} do
-      stockable_id = uuid4()
       to_streams("stockable", [
         %StockableAdded{
-          stockable_id: stockable_id,
+          stockable_id: cmd.stockable_id,
           name: Faker.Commerce.product_name(),
           unit_of_measure: "EA"
         }
       ])
 
-      cmd = %{cmd | requester_role: "system", stockable_id: stockable_id}
+      cmd = %{cmd | requester_role: "system"}
 
       assert :ok = Router.dispatch(cmd)
       assert_receive_event(StockableUpdated, fn(event) ->
